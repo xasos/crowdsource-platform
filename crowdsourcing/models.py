77fd1df6ef285ab84b5b75d16539b0e4101d92a2
srxzr <milad@Milad-MacBook-Pro.local>
2015-04-20 19:55:36 +0430

    Skill and Skill test added
    docstringing in progress :D

--- crowdsourcing/models.py
+++ crowdsourcing/models.py
@@ -77,6 +77,32 @@ class Skill(models.Model):
     deleted = models.BooleanField(default=False)
 
 
+
+class Questions(models.Model):
+    _content=models.TextField()
+    """Type of question 0=(detail answer)  1=(Yes/No) 2< number of options   """
+    type=models.IntegerField ()
+    hardness = models.IntegerField()
+
+
+
+class Test(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+
+
+class SkillTest(models.Model):
+    skill=models.ForeignKey(Skill)
+    test=models.ForeignKey(Test)
+
+
+class TestQuestion(models.Model):
+    question= models.ForeignKey(Questions)
+    test= models.ForeignKey(Test)
+
+
+
+
 class Worker(UserProfile):
     skills = models.ManyToManyField(Skill, through='WorkerSkill')
 
