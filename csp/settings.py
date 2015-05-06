00c00587a4804185a3616c1416db3f5b3169b8c0
Kunal Khandelwal <kunal15595@gmail.com>
2015-05-06 13:20:39 +0530

    overwrite database settings in settings.py

--- csp/settings.py
+++ csp/settings.py
@@ -120,8 +120,16 @@ WSGI_APPLICATION = 'csp.wsgi.application'
 #         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+# DATABASES = {
+#     'default': dj_database_url.config()
+# }
+
 DATABASES = {
-    'default': dj_database_url.config()
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "crowdsource_dev"
+    }
 }
 
 # Internationalization
