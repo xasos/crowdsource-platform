2abaa4c75168257fc9462db73e32b75c4c0f1203
Kunal Khandelwal <kunal15595@gmail.com>
2015-05-07 10:29:17 +0530

    added node modules to gitignore

--- csp/settings.py
+++ csp/settings.py
@@ -127,7 +127,7 @@ WSGI_APPLICATION = 'csp.wsgi.application'
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "ENGINE": "django.db.backends.sqlite3",
         "NAME": "crowdsource_dev"
     }
 }
